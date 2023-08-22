c67f6de06fab7962564aae5a9b185e712688b0ae
AR <rivierealan4@gmail.com>
2023-08-22 15:19:37 +0200

    Publication du projet
    Publication des fichiers liés au projet du module de Graphes réalisé au cours du deuxième semestre de la 3ème année de Licence informatique.

---
+++ ProjetGraphe.cpp
@@ -0,0 +1,179 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <time.h>
+
+using namespace std;
+const int n=5;     //Graphe à n sommets
+int adj[n][n];  // matrice d’adjacence du graphe
+int W[n][n];   //Matrice après l'algo FloydWarshall
+int Pexacte[n][n];
+vector< vector<int> > listClique;
+
+vector<int> Intersect(vector<int> A, vector<int> B){    //Fais l'intersection de deux liste
+    vector<int> I;
+    for(int i = 0; i<A.size(); i++){
+        for(int j = 0; j<B.size(); j++){
+            if(A[i]==B[j]) I.push_back(A[i]);
+        }
+    }
+    return I;
+}
+
+vector<int> Suppr(vector<int> A, int b){    //Supprime le sommet b de la liste
+    for(int i = 0; i<A.size(); i++){
+        if(A[i]==b) A.erase(A.begin()+i);
+    }
+    return A;
+}
+
+vector<int> GetV(int v){    //Obtient les voisin du sommet v
+    vector<int> V;
+    for(int i = 0; i<n; i++){
+        if(adj[v][i]==1) V.push_back(i);
+        //if(Pexacte[v][i]==1) V.push_back(i);
+    }
+    return V;
+}
+
+void BronKerbosch(vector<int> R, vector<int> P, vector<int> X){
+    if((P.size() == 0) && (X.size() == 0) && (R.size()>1)){
+        printf("Clique maximale :");
+        vector<int> clique;
+        for (int i=0; i<R.size(); i++){
+            printf(" %d ", R[i]);
+            clique.push_back(R[i]);
+        }
+        printf("\n");
+        listClique.push_back(clique);
+        return;
+    }
+
+    vector<int> P1;
+    for(int i = 0; i<P.size();i++){P1.push_back(P[i]);}
+
+    for(int i = 0; i < P.size(); i++){
+        int v = P[i];
+        vector<int> N = GetV(v);
+        R.push_back(v);
+        BronKerbosch(R,Intersect(P1,N),Intersect(X,N));
+        P1 = Suppr(P1,v);
+        R = Suppr(R,v);
+        X.push_back(v);
+    }
+}
+
+
+//_____________________________________________________________________________________________________________________________
+void GenereGraphe(int p){ //p est la probabilité que deux sommets soit reliés
+    srand(time(NULL));
+    int x;
+    for (int i=0;i<n;i++){
+        for (int j=n-1;j>=i;j--){
+            x=rand()%101;
+            if(x<=p && i!=j){
+                adj[i][j]=1;
+                adj[j][i]=1;
+
+                W[i][j]=1;
+                W[j][i]=1;
+            }
+			else{
+                W[i][j]=n; //n représente l'infini ici
+                W[j][i]=n; //il n'y aura jamais n points reliés à la suite
+			}
+        }
+    }
+}
+
+void AfficheAdj(int tab[n][n]) //Affiche les matrices d'adjacence
+{
+    for(int i=0;i<n;i++)
+	{
+        for(int j=0;j<n;j++)
+		{
+            if(tab[i][j]==n) //n pour l'infini
+			{
+				printf("inf  ");
+			}
+			else printf(" %d   ",tab[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void FloydWarshall(){
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+                if(i==j){ W[i][j]= 0; }
+				else{ if(W[i][k]+W[k][j] <= W[i][j]){ W[i][j]= W[i][k]+W[k][j];}}
+			}
+        }
+    }
+}
+
+void PuissanceExacte(int d){
+	FloydWarshall();
+	for (int i=0;i<n;i++){
+        for (int j=n-1;j>=i;j--){
+            if(W[i][j]==d){
+                Pexacte[i][j]=1;
+                Pexacte[j][i]=1;
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]){
+
+    GenereGraphe(50);
+    PuissanceExacte(2);
+
+    printf("Matrice d'adjacence\n");
+    AfficheAdj(adj);
+
+    printf("\n\nPlus courte distance\n");
+    AfficheAdj(W);
+
+    printf("\n\nPuissance exacte\n");
+    AfficheAdj(Pexacte);
+
+    printf("\n\nListe des cliques maximales\n");
+    vector<int> R;
+    vector<int> P;
+    for (int i=0; i<n; i++){
+        P.push_back(i);
+    }
+    vector<int> X;
+    BronKerbosch(R,P,X);
+
+    int tailleCliqM = 0;
+    for (int i=0; i<listClique.size(); i++)
+    {
+        if (listClique[i].size() > tailleCliqM) tailleCliqM = listClique[i].size();
+    }
+    printf("\nLa taille de la clique maximum est : %d\n",tailleCliqM);
+
+    ofstream fichier("Clique.txt", ios::out | ios::trunc);
+    if(fichier)
+    {
+        for (int j=0; j<listClique.size(); j++)
+        {
+            if (listClique[j].size() == tailleCliqM) {
+                for (int k=0; k<listClique[j].size(); k++)
+                {
+                    fichier << listClique[j][k] << "\t";
+                }
+                fichier << endl;
+            }
+        }
+        fichier.close();
+    } else {
+        cerr << "Erreur à l'ouverture !" << endl;
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Projet_Graphes_L3-S2
- Projet réalisé durant le module de Graphes du 2ème semestre de la 3ème année de licence informatique
+# Projet de Graphes (L3 semestre 2)
+
+Le but du projet était d’implanter l’algorithme de Bron-Kerbosch pour afficher toutes les cliques maximum d’un graphe non orienté, puis de l’appliquer sur des graphes généré aléatoirement. 
+
+## Génération de graphe aléatoire
+
+Il fut décidé de commencer par programmer la génération de graphe aléatoire afin de pouvoir s’en servir pour tester les méthodes ***FloydWarshall*** et ***BronKerbosch***.
+
+La fonction ***Generegraphe*** prend en paramètre la probabilité (comprise entre 0 et 100 inclus) qu’il y ai ou non une arrête entre deux sommets. 
+
+Cette méthode remplie deux matrices d’adjacence :
+- **adj** (de taille **n** * **n**) pour la méthode ***Bronkerbosh*** avec des 1 en cas d’arêtes et 0 sinon
+- **W** (de taille **n** * **n**) identique à **adj** à ceci près que la valeur **n** remplace les 0 étant donné que l’algorithme de Floyd-Warshall fonctionne par comparaisons de valeurs infinies. La distance maximale entre 2 points étant de **n**-1, la valeur **n** a été choisie pour représenter l’infini.
+
+## Puissance éxacte d’un graphe
+
+La méthode ***FloydWarshall*** permet de remplir la matrice **W** avec les plus cours chemins entre chaque pair de sommets.
+
+Une fois la matrice de plus courts chemins réalisée, il ne reste plus qu’à afficher les distances égales à la puissance exacte souhaitée via l’appelle de notre fonction ***PuissanceExacte*** où le paramètre **d** est un entier indiquant la valeur de la puissance exacte souhaité, cette fonction remplie la matrice de puissance exacte **Pexacte** de la manière suivante :
+* 1 si la plus courte distance est égal au paramètre **d**
+* 0 dans le cas contraire
+
+## Algo de Bron-Kerbosh
+
+La méthode ***BronKerbosch*** réalisée permet d’afficher la liste de toutes les cliques maximales, en outre elle remplie une liste stockant les cliques maximales rencontrées afin de déterminer la ou les cliques maximum ainsi que leur taille.
+Les variables passées en paramètres de cette méthode sont des listes de sommets utilisé lors de l’exécution récursive de la manière suivante :
+    • **R** contient les sommets faisant déjà partie de la clique.
+    • **P** contient tous les sommets voisins du sommet en cours de traitement.
+    • **X** contient les sommets qui ont déjà été utilisé dans une précédente clique.
+
+Cette méthode se sert également des fonctions :
+    • ***Intersect*** pour réaliser les opérations d’intersection entre les différentes listes.
+    • ***Suppr*** pour supprimer l’un des sommets de la liste passé en paramètre.
+    • ***GetV*** pour récupérer l’ensemble des sommets voisins de celui passé en paramètre.
